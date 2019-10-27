2c7ec9d428dc1228c6c4593c39053f375a5ab5df
David Keipert <hello@davidkeipert.me>
2019-10-26 20:29:27 -0500

    more work on the basic structure of the bot.  Added app.yaml for GCP deployment

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
+const request = require('request');
 
 client.on('ready', () => {
     console.log("I am ready!");
 })
 
-client.on('message', message => {
+client.on('message', async message => {
+
+    if(message.author.bot) return;
+
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    const args  = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping") {
+        const m = await message.channel.send("Ping");
+        m.edit('Pong! Latency is ${m.createdTimeStamp - message.createdTimeStamp} ms. API latency is ${Math.round(client.ping)}ms')
+    }
+
+
     if (message.content == "ping") {
         message.reply("pong");
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login();
