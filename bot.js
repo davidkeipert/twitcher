f78287d75de3c35de7828685aa15844583435929
David Keipert <hello@davidkeipert.me>
2019-10-26 23:12:36 -0500

    implemented twitch userID lookup with the !follow command

--- bot.js
+++ bot.js
@@ -1,10 +1,22 @@
+//import { getUserID } from './twitchUtils';
+//const getUserID = require('./twitchUtils.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-const request = require('request');
+const fetch = require("node-fetch")
 
 client.on('ready', () => {
-    console.log("I am ready!");
+    console.log(`Bot started, ${client.users.size} users, active in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+});
+
+client.on('guildCreate', guild => {
+    console.log(`Joined guild: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers.`);
+});
+
+client.on('guildDelete', guild => {
+    console.log(`Bot has been removed from ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers.`);
 })
 
 client.on('message', async message => {
@@ -17,15 +29,34 @@ client.on('message', async message => {
 
     const command = args.shift().toLowerCase();
 
+    console.log(args);
+
     if(command === "ping") {
         const m = await message.channel.send("Ping");
-        m.edit('Pong! Latency is ${m.createdTimeStamp - message.createdTimeStamp} ms. API latency is ${Math.round(client.ping)}ms')
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp} ms. API latency is ${Math.round(client.ping)}ms`);
     }
 
+    if(command === "follow") {
+        var twitchUser = args[0];
+        var userID = getUserID(twitchUser);
 
-    if (message.content == "ping") {
-        message.reply("pong");
+        
     }
+    
+
 });
 
-client.login();
+client.login(config.token);
+
+function getUserID(userName) {
+    var url = "https://api.twitch.tv/helix/users?login=" + userName;
+    fetch(url, {
+        headers: {
+            'Client-ID': `${config.twitchClientID}`
+        }
+    }).then(resp => resp.json()).then(response => {
+        
+        console.log(`User ID for ${userName} is ${response.data[0].id}`);
+        return(response.data[0].id);
+    })
+}
