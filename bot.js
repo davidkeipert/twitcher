5771cbe67fcdc3c7e46304c7eff87897d3b0e423
David Keipert <qhorse18@icloud.com>
2019-10-28 05:01:41 +0000

    basic webhook functionality for detecting follows

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-//import { getUserID } from './twitchUtils';
-//const getUserID = require('./twitchUtils.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 const fetch = require("node-fetch");
-const http = require('http');
+const express = require('express');
+const http = require('http')
+
 
 client.on('ready', () => {
     console.log(`Bot started, ${client.users.size} users, active in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
@@ -24,28 +24,43 @@ client.on('guildDelete', guild => {
 
 client.on('message', async message => {
 
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
-    if(message.content.indexOf(config.prefix) !== 0) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
 
-    const args  = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 
     const command = args.shift().toLowerCase();
 
     console.log(args);
 
-    if(command === "ping") {
+    if (command === "ping") {
         const m = await message.channel.send("Ping");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp} ms. API latency is ${Math.round(client.ping)}ms`);
     }
 
-    if(command === "follow") {
+    if (command === "follow") {
         var twitchUser = args[0];
         var userID = getUserID(twitchUser);
 
+        //start an Express server and listen for the GET verification request from twitch
+
+        const server = express();
+
+        server.get('/', (req, res) => {
+            var challenge = req.query['hub.challenge'];
+            console.log(challenge)
+            res.send(challenge);
+            console.log('response sent')
+        }).listen(80, () => console.log('Server listening on port 80'));
+
+        //POST the subscription request
+        console.log('fetching')
+
+
         var url = 'https://api.twitch.tv/helix/webhooks/hub';
         const data = {
-            'hub.callback': 'https://dk-domain.appspot.com/',
+            'hub.callback': 'http://51.158.177.181/',
             'hub.mode': 'subscribe',
             'hub.topic': 'https://api.twitch.tv/helix/users/follows?first=1&to_id=416145352', //temporarily hardcoded user ID
             'hub.lease_seconds': '864000',
@@ -61,15 +76,17 @@ client.on('message', async message => {
             }
         })
 
-        var server = http.createServer((request, result) => {
-            if(request.method === "GET") {
-                
-            }
+        server.post('/', (req, res) => {
+            console.log('webhook notification received');
+            res.sendStatus(200);
+
+            client.channels.find("name","twitch-alerts").send("new follow detected")
+
         })
 
 
     }
-    
+
 
 });
 
@@ -82,8 +99,8 @@ function getUserID(userName) {
             'Client-ID': `${config.twitchClientID}`
         }
     }).then(resp => resp.json()).then(response => {
-        
+
         console.log(`User ID for ${userName} is ${response.data[0].id}`);
-        return(response.data[0].id);
+        return (response.data[0].id);
     })
 }
